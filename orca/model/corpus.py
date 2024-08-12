e0510838916c8dbc3088d07294495652456d0df5
Sean Gilleran <sgilleran@gmail.com>
2024-08-12 16:20:42 -0700

    Fixed erroneous upload, changed hashing algorithm

--- orca/model/corpus.py
+++ orca/model/corpus.py
@@ -20,8 +20,8 @@ class Corpus(Base):
 
     __tablename__ = "corpuses"
     hash = Column(String, primary_key=True)
-    created = Column(DateTime, nullable=False, default=get_utcnow)
     total = Column(Integer, nullable=False)
+    created = Column(DateTime, nullable=False, default=get_utcnow)
     documents = relationship(
         "Document", back_populates="corpuses", secondary=corpus_table
     )
@@ -35,22 +35,20 @@ class Corpus(Base):
         """Take a snapshot of the current tables and save."""
         documents = Document.get_all(session=session)
         total = len(documents)
+        log.info(f"Adding {total} documents to corpus")
 
         # Generate and store hash value
-        log.info(f"Hashing {total} documents")
-        hash = hashlib.sha256(
-            "".join([d.id for d in documents]).encode(),
-            usedforsecurity=False,
-        ).hexdigest()
-
-        log.info(f"Adding {total} documents to corpus")
-        corpus = cls(hash=hash, documents=documents, total=len(documents))
+        log.info("Hashing document contents, this may take some time")
+        raw = "".join([d.content for d in documents])
+        hash = hashlib.sha256(raw.encode(), usedforsecurity=False).hexdigest()
+        corpus = cls(hash=hash, documents=documents, total=total)
         session.add(corpus)
+
         for i, document in enumerate(documents):
             document.corpus = corpus
             session.add(document)
-            if (i + 1) % config.DATABASE_BATCH_SIZE == 0 or (i + 1) == total:
-                log.info(f"Adding documents to corpus ({i + 1}/{total})")
+            if (i + 1) % config.APP_NAME0 == 0 or (i + 1) == total:
+                log.info(f"Adding document to corpus ({i + 1}/{total})")
                 session.commit()
 
         log.info("Done creating corpus")
@@ -58,17 +56,26 @@ class Corpus(Base):
 
     @classmethod
     @with_session
+    def get_all(cls, session=None):
+        result = session.query(cls).order_by(desc(cls.created)).all()
+        if not result:
+            log.warning("Tried getting all corpuses but none exist")
+        return result
+
+    @classmethod
+    @with_session
     def get_latest(cls, session=None):
         """Return the most recent corpus."""
         result = session.query(cls).order_by(desc(cls.created)).first()
         if not result:
-            log.warning("Tried getting most recent corpus snapshot but none exist")
+            log.warning("Tried getting most recent corpus but none exist")
         return result
 
     def as_dict(self):
         return {
             "hash": self.hash,
             "color": "#" + self.hash[:6],
-            "documents": self.total,
+            "total": self.total,
+            "created": self.created.isoformat() + "Z",
             "searches": [s.as_dict() for s in self.searches],
         }
